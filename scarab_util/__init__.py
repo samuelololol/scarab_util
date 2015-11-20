250086f0193faf7f93848fc870c2e98bbf958fcb
samuelololol <samuelololol@gmail.com>
2015-11-20 16:11:20 +0800

    add argparser

--- scarab_util/__init__.py
+++ scarab_util/__init__.py
@@ -3,9 +3,26 @@
 __date__= 'Nov 20, 2015 '
 __author__= 'samuel'
 
+import argparse
+from argparse import RawTextHelpFormatter
+import textwrap
 
 def main():
-    print "hello scarab_util"
+    #print "hello scarab_util"
+    #pass
+    description = """\
+    CLI tool for scarab web framework.
+
+    scarab -a generate 
+    scarab -a show
+    ...
+    """
+    usage = "usage: %prog -a {action} [parameters]"
+    parser = argparse.ArgumentParser(
+            description=textwrap.dedent(description),
+            formatter_class=RawTextHelpFormatter)
+            #description=description)
+    args = parser.parse_args()
 
 if __name__ == '__main__':
     main()
