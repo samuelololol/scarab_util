95c179810dbbec0b3df26dd22159c17dead7652f
samuelololol <samuelololol@gmail.com>
2015-11-20 18:00:02 +0800

    update command parser

--- scarab_util/__init__.py
+++ scarab_util/__init__.py
@@ -6,34 +6,90 @@ __author__= 'samuel'
 import argparse
 from argparse import RawTextHelpFormatter
 import textwrap
+import sys
+import os
 
 def main():
-    description = """\
-    CLI tool for scarab web framework.
+    ScarabCmd()
 
-    scarab generate 
-    scarab show
-    ...
-    """
-    #usage = "usage: %prog <command> [parameters]"
-    # top-level parser
-    parser = argparse.ArgumentParser(
-            description=textwrap.dedent(description),
-            formatter_class=RawTextHelpFormatter)
 
-    subparsers = parser.add_subparsers(help='sub-command help')
+class ScarabCmd(object):
+    def __init__(self):
+        description = "CLI tool for scarab web framework"
+        usage = """scarab <command> [<args>]
 
-    # generate command
-    generate_parser = subparsers.add_parser('generate', help='generate help')
-    generate_parser.add_argument(
-            '-t', '--type',
-            required=True,
-            type=str,
-            choices=['api','page','service','model'],
-            help='component type')
+        The available commands are:
+        generate  Generate API server component
+        show      Show API server related status
+        """
+        parser = argparse.ArgumentParser(
+                description=textwrap.dedent(description),
+                formatter_class=RawTextHelpFormatter,
+                usage=usage,
+                )
+        parser.add_argument('command', help='Subcommand to run')
+        args = parser.parse_args(sys.argv[1:2])
+        if not hasattr(self, args.command):
+            print 'Unrecognized command'
+            parser.print_help()
+            exit(1)
+        return getattr(self, args.command)()
 
-    args = parser.parse_args()
-    print args
+    def generate(self):
+        description='Generate API server component'
+        usage = """scarab generate -t TYPE [<args>]
+
+        The available TYPEs are:
+        api         Add route, api, service, and test
+        page        Add route, page, service, and test
+        model       Add model, initial script, and test
+        """
+
+        parser = argparse.ArgumentParser(
+                description=textwrap.dedent(description),
+                formatter_class=RawTextHelpFormatter,
+                usage=usage,
+                )
+
+        parser.add_argument('-t', '--type',
+                            required=True,
+                            type=str,
+                            choices=['api', 'page', 'model'],
+                            help='component type')
+        args = parser.parse_args(sys.argv[2:])
+        print args
+
+
 
 if __name__ == '__main__':
     main()
+    
+#deprecated
+#def main():
+    #description = "CLI tool for scarab web framework"
+    #usage = """scarab <command> [<args>]
+
+    #The available commands are:
+    #generate  Generate API server component
+    #show      Show API server related status
+    #"""
+
+    ## top-level parser
+    #parser = argparse.ArgumentParser(
+    #        description=textwrap.dedent(description),
+    #        formatter_class=RawTextHelpFormatter,
+    #        usage=usage)
+
+    #subparsers = parser.add_subparsers(help='sub-command help')
+
+    ## generate command
+    #generate_parser = subparsers.add_parser('generate', help='generate help')
+    #generate_parser.add_argument(
+    #        '-t', '--type',
+    #        required=True,
+    #        type=str,
+    #        choices=['api','page','service','model'],
+    #        help='component type')
+
+    #args = parser.parse_args()
+    #print args
