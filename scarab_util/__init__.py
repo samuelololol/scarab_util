5544cc3e03efc0eccdf77ad06623bb39115f4e8f
samuelololol <samuelololol@gmail.com>
2015-11-20 18:15:16 +0800

    remove deprecated.

--- scarab_util/__init__.py
+++ scarab_util/__init__.py
@@ -12,15 +12,15 @@ import os
 def main():
     ScarabCmd()
 
-
 class ScarabCmd(object):
     def __init__(self):
         description = "CLI tool for scarab web framework"
         usage = """scarab <sub_command> [<args>]
 
-        The available sub_commands are:
-        generate  Generate API server component
-        show      Show API server related status
+        Available <sub_command> are:
+
+        generate    Generate API server component
+        show        Show API server related status
         """
         parser = argparse.ArgumentParser(
                 description=textwrap.dedent(description),
@@ -37,20 +37,10 @@ class ScarabCmd(object):
 
     def generate(self):
         description='Generate API server component'
-        #usage = """scarab generate -t TYPE [<args>]
-
-        #The available TYPEs are:
-        #api         Add route, api, service, and test
-        #page        Add route, page, service, and test
-        #model       Add model, initial script, and test
-        #"""
-
         parser = argparse.ArgumentParser(
                 description=textwrap.dedent(description),
                 formatter_class=RawTextHelpFormatter,
-                #usage=usage,
                 )
-
         parser.add_argument('-t', '--type',
                             required=True,
                             type=str,
@@ -65,39 +55,10 @@ class ScarabCmd(object):
         print args
 
     def show(self):
-        print 'under construction'
-
+        print """scarab show: under construction
+        """
 
 
 if __name__ == '__main__':
     main()
-    
-#deprecated
-#def main():
-    #description = "CLI tool for scarab web framework"
-    #usage = """scarab <command> [<args>]
 
-    #The available commands are:
-    #generate  Generate API server component
-    #show      Show API server related status
-    #"""
-
-    ## top-level parser
-    #parser = argparse.ArgumentParser(
-    #        description=textwrap.dedent(description),
-    #        formatter_class=RawTextHelpFormatter,
-    #        usage=usage)
-
-    #subparsers = parser.add_subparsers(help='sub-command help')
-
-    ## generate command
-    #generate_parser = subparsers.add_parser('generate', help='generate help')
-    #generate_parser.add_argument(
-    #        '-t', '--type',
-    #        required=True,
-    #        type=str,
-    #        choices=['api','page','service','model'],
-    #        help='component type')
-
-    #args = parser.parse_args()
-    #print args
