dae7f6842c8e2f0e5cb11171cd579ceb2f660e9c
samuelololol <samuelololol@gmail.com>
2015-11-21 22:55:49 +0800

    add service works

--- scarab_util/generate/service_class.py
+++ scarab_util/generate/service_class.py
@@ -3,7 +3,28 @@
 __date__= 'Nov 21, 2015 '
 __author__= 'samuel'
 
+import os
+import jinja2
 
 def add_new_service(folder_root_path, service_name):
-    print 'new service'
+    print 'Generating service ... ',
+    try:
+        templateLoader = jinja2.FileSystemLoader(searchpath="/")
+        templateEnv = jinja2.Environment(loader=templateLoader)
+
+        API_TEMPLATE_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                'service_template.jinja2')
+        template = templateEnv.get_template(API_TEMPLATE_FILE)
+
+        service_file_path = os.path.join(os.path.join(folder_root_path, 'services'), service_name + '.py')
+        variables = {'service_name': service_name}
+        outputText = template.render(variables)
+        with open(service_file_path, 'wb') as f:
+            f.write(outputText)
+    except Exception, e:
+        print 'fail'
+        print e
+        return False
+    print 'done'
+    return True
 
--- scarab_util/test/generate/test_api.py
+++ scarab_util/test/generate/test_api.py
@@ -10,30 +10,46 @@ from scarab_util.test.common.mock_scarab import ScarabRoute
 
 from scarab_util import ScarabCmd
 
-def test_add_route(ScarabRoute, ScarabRootPath):
+@pytest.fixture(scope='function')
+def generated_API(request, ScarabRoute, ScarabRootPath):
     check_type = 'api'
     check_route = '/myapi'
     check_name = 'test'
     command_string = ['scarab', 'generate', '-t', check_type, '-p', check_route, '-n', check_name, '-r', ScarabRootPath]
     ScarabCmd(command_string)
+
+    return {'check_type': check_type,
+            'check_route': check_route,
+            'check_name': check_name,
+            'command_string': command_string
+            }
+
+def test_add_route(ScarabRoute, generated_API):
+    check_route = generated_API['check_route']
 
     route_line = ''
     with open(ScarabRoute, 'r') as routepy:
         for line in routepy.readlines():
             if check_route in line:
                 route_line = line
-    print route_line
     assert route_line != ''
+    print route_line
 
-def test_add_api(ScarabRootPath, ScarabRoute):
-    check_type = 'api'
-    check_route = '/myapi'
-    check_name = 'test'
-    command_string = ['scarab', 'generate', '-t', check_type, '-p', check_route, '-n', check_name, '-r', ScarabRootPath]
-    ScarabCmd(command_string)
+def test_add_api(ScarabRootPath, generated_API):
+    check_name = generated_API['check_name']
+
     api_file_path = os.path.join(os.path.join(ScarabRootPath, 'apis'), check_name + '.py')
     print api_file_path
     assert True == os.path.isfile(api_file_path)
     with open(api_file_path, 'r') as f:
+        print f.read()
+
+def test_add_service(ScarabRootPath, generated_API):
+    check_name = generated_API['check_name']
+
+    service_file_path = os.path.join(os.path.join(ScarabRootPath, 'services'), check_name + '.py')
+    print service_file_path
+    assert True == os.path.isfile(service_file_path)
+    with open(service_file_path, 'r') as f:
         print f.read()
 
