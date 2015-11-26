b5325c5dcadbf18cb8aa44bfcb259b15be5a08b8
samuelololol <samuelololol@gmail.com>
2015-11-26 13:37:54 +0800

    Update setup.py
    follow: https://pytest.org/latest/goodpractises.html#integration-with-setuptools-test-commands

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 __date__= 'Nov 20, 2015 '
 __author__= 'samuel'
+
 import os
+import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
@@ -10,22 +12,31 @@ README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 
-
 requires = ['jinja2',
             'pytest-capturelog',
             'pytest-cov',
             'pytest',
            ]
 
+
 class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', 'Arguments to pass to py.test')]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = ["-s"]
+        #self.test_args = ["-s", "-m", "voice"]
+        self.test_args = []
         self.test_suite = True
 
     def run(self):
         import pytest
-        pytest.main(self.test_args)
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 setup(name='scarab_util',
       version=VERSION,
